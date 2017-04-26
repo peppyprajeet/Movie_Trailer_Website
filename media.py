35a827404deaedfc37dda054f486d704950480ab
Prajeet Kumar Patibandla <peppyprajeet@users.noreply.github.com>
2017-04-26 16:52:37 -0400

    media.py

--- media.py
+++ media.py
@@ -7,7 +7,8 @@ class Movie():
     #Class Varibales - declared outside init function - declared in CAPS as it is constant
     VALID_RATINGS = ["G", "PG", "PG-13", "R"] 
 
-    #Constructor
+    #Constructor : the init fucntion is instantiated here associated to class Movie
+    #This function takes the input values that are associated with the movie titles, movie storylines, poster image and trailer. 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         #Instance Variables
@@ -16,6 +17,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #Instance Methods
+    #Instance Methods : the oinstance method with refernce to self is declared so that calling the function to open the trailer in web browser
     def show_trailer(self):
             webbrowser.open(self.trailer_youtube_url)
